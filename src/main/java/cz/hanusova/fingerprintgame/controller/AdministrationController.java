f5aa907a1eb107d2e18304eed71a1d016e6f84ca
khanusova90 <k.hanusova90@gmail.com>
2015-12-05 18:19:21 +0100

    Funkcni controller

--- src/main/java/cz/hanusova/fingerprintgame/controller/AdministrationController.java
+++ src/main/java/cz/hanusova/fingerprintgame/controller/AdministrationController.java
@@ -4,11 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value = "/administration")
 public class AdministrationController {
 
-	@RequestMapping(value = "administration")
-	public void getRequest() {
+	@RequestMapping(value = "/")
+	public String getRequest() {
 		System.out.println("CONTROLLER");
+
+		return "administration";
+	}
+
+	@RequestMapping(value = "/test")
+	public String getRequesttest() {
+		System.out.println("test");
+
+		return "administration";
 	}
 
 }
