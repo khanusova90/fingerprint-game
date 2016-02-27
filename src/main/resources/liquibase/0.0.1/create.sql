51c3852a4f102fa38fc8cdd5ff25914059642041
khanusova90 <k.hanusova90@gmail.com>
2016-02-27 17:39:46 +0100

    ab

---
+++ src/main/resources/liquibase/0.0.1/create.sql
@@ -0,0 +1,48 @@
+--liquibase formatted sql
+
+--changeset hanuska1:create-1
+drop table if exists app_user;
+drop table if exists inventory;
+drop table if exists place;
+drop table if exists resources;
+
+create table APP_USER(
+	ID_USER bigint(19) not null auto_increment,
+	USERNAME varchar(255) not null,
+	STAGNAME varchar(255) not null,
+	PASSWORD varchar(255) not null,
+	primary key (ID_USER)
+);
+create table INVENTORY(
+	ID_INVENTORY bigint(19) not null auto_increment,
+	ID_USER bigint(19) not null,
+	MATERIAL varchar(255) not null,
+	AMOUNT numeric(19,2) not null default 0,
+	primary key (ID_INVENTORY),
+	constraint fk_inventory_user foreign key fk_inventory_user(ID_USER) references APP_USER(ID_USER)
+);
+create table PLACE(
+	ID_PLACE bigint(19) not null auto_increment,
+	CODE varchar(255) not null,
+	DESCRIPTION varchar(255) not null,
+	primary key (ID_PLACE)
+);
+create table RESOURCES(
+	ID_RESOURCES bigint(19) not null auto_increment,
+	ID_PLACE bigint(19) not null,
+	MATERIAL varchar(255) not null,
+	primary key(ID_RESOURCES),
+	constraint fk_resources_place foreign key fk_resources_place(ID_PLACE) references PLACE(ID_PLACE)
+);
+--rollback drop table if exists app_user; drop table if exists inventory; drop table if exists place; drop table if exists resources;
+
+--changeset hanuska1:create-2
+drop table if exists user_role;
+
+create table USER_ROLE(
+	ID_USER_ROLE bigint(19) not null auto_increment,
+	ROLE varchar(255) not null,
+	ID_USER bigint(19) not null,
+	primary key(ID_USER_ROLE),
+	constraint fk_user_roles foreign key fk_user_roles(ID_USER) references APP_USER(ID_USER)
+);
