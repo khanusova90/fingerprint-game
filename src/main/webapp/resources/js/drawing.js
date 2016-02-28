20ebb4ebaad3eb5ecd6675dbf8e6834e1966787b
khanusova90 <k.hanusova90@gmail.com>
2016-02-28 16:40:04 +0100

    Added fragments, drawing into canvas

--- src/main/webapp/resources/js/drawing.js
+++ src/main/webapp/resources/js/drawing.js
@@ -1,3 +1,25 @@
+var canvas, context;
+
+function drawMap(){
+	canvas = document.getElementById("map");
+	ch = canvas.getAttribute("height");
+	cw = canvas.getAttribute("width");
+	
+	context = canvas.getContext("2d");
+	img = document.getElementById("imgsrc");
+	ih = img.getAttribute("height");
+	iw = img.getAttribute("width");
+	
+//	context.drawImage(img, 0, 0, cw, ch, 0, 0, iw, ih);
+	context.drawImage(img, 0, 0);
+	
+//	baseImage = new Image(500, 500);
+//	baseImage.src = "resources/img/J1NP.jpg";
+//	baseImage.onload = function(){
+//		context.drawImage(baseImage, 500, 500);
+//	};
+};
+
 //window.onload = function () {
 //	var map = document.getElementById("map");
 //	map.setAttribute('height', 500);
@@ -19,30 +41,30 @@
 //	ctx.fill();
 //}
 
-jQuery(document).ready(function() {
-  $('#map').attr('height', $('#map').css('height'));
-  $('#map').attr('width', $('#map').css('width'));
-
-  $("#map").click(function(e) {
-
-    var x = e.clientX - this.offsetLeft - this.scrollLeft;
-    var y = e.clientY - this.offsetTop - this.scrollTop;
-//	  var x = e.pageX;
-//	  var y = e.pageY;
-    
-    //var pole = [];
-    //pole.push[{
-
-    var ctx = this.getContext("2d");
-    ctx.beginPath();
-    ctx.arc(x, y, 10, 0, 2 * Math.PI);
-    ctx.stroke();
-    
-    //json object vratit
-    //musi se jmenovat stejne
-  });
-  
-  //na kliknuti na odkaz mapy zobrazit konkretni patro 
-  
-  //podivat se na metodu drawImage()
-})
+//jQuery(document).ready(function() {
+//  $('#map').attr('height', $('#map').css('height'));
+//  $('#map').attr('width', $('#map').css('width'));
+//
+//  $("#map").click(function(e) {
+//
+//    var x = e.clientX - this.offsetLeft - this.scrollLeft;
+//    var y = e.clientY - this.offsetTop - this.scrollTop;
+////	  var x = e.pageX;
+////	  var y = e.pageY;
+//    
+//    //var pole = [];
+//    //pole.push[{
+//
+//    var ctx = this.getContext("2d");
+//    ctx.beginPath();
+//    ctx.arc(x, y, 10, 0, 2 * Math.PI);
+//    ctx.stroke();
+//    
+//    //json object vratit
+//    //musi se jmenovat stejne
+//  });
+//  
+//  //na kliknuti na odkaz mapy zobrazit konkretni patro 
+//  
+//  //podivat se na metodu drawImage()
+//})
