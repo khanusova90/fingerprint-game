e636a35f2e35662d402a9c6ccfee04e1d87a4abc
Kateřina Hanušová <k.hanusova90@gmail.com>
2017-03-20 07:10:18 +0100

    630920 - Lazy initialization fix

--- src/test/java/cz/hanusova/fingerprintgame/service/ItemServiceTest.java
+++ src/test/java/cz/hanusova/fingerprintgame/service/ItemServiceTest.java
@@ -35,6 +35,8 @@ public class ItemServiceTest {
 	private ItemTypeRepository itemTypeRepositoryMock;
 	@Mock
 	private InventoryService inventoryServiceMock;
+	@Mock
+	private UserService userServiceMock;
 
 	private UserBuilder userBuilder;
 	private ItemBuilder itemBuilder;
@@ -43,7 +45,7 @@ public class ItemServiceTest {
 	public void init() {
 		MockitoAnnotations.initMocks(this);
 		itemService = new ItemServiceImpl(userRepositoryMock, itemTypeRepositoryMock, itemRepositoryMock,
-				inventoryServiceMock);
+				inventoryServiceMock, userServiceMock);
 		userBuilder = new UserBuilder();
 		itemBuilder = new ItemBuilder();
 	}
@@ -51,29 +53,36 @@ public class ItemServiceTest {
 	@Test
 	public void addItemTest() {
 		AppUser user = userBuilder.build();
+		signInUser(user);
 		int itemSize = user.getItems().size();
 		Item item = itemBuilder.build(1);
-		itemService.addItem(user, item);
+		itemService.addItem(item);
 		Assert.assertNotEquals("User should get new item to his item list", itemSize, user.getItems().size());
 	}
 
 	@Test
 	public void addItemUpdateTest() {
 		AppUser user = userBuilder.build();
+		signInUser(user);
 		Item item1 = itemBuilder.build(1);
 		user.getItems().add(item1);
 		Assert.assertEquals("User should have only one item", 1, user.getItems().size());
 		Item item2 = itemBuilder.build(2);
-		itemService.addItem(user, item2);
+		itemService.addItem(item2);
 		Assert.assertEquals("Size of item list should not change", 1, user.getItems().size());
 	}
 
 	@Test
 	public void addItemInventoryUpdateTest() {
 		AppUser user = userBuilder.build();
+		signInUser(user);
 		Item item = itemBuilder.build(1);
-		itemService.addItem(user, item);
+		itemService.addItem(item);
 		Mockito.verify(inventoryServiceMock, Mockito.times(1)).updateGoldAmount(Mockito.anyFloat(), Mockito.any());
+	}
+
+	private void signInUser(AppUser user) {
+		Mockito.when(userServiceMock.getActualUser()).thenReturn(user);
 	}
 
 }
