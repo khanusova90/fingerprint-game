d8de7a181f764253132af7f023361ddbc8e1b999
Kateřina Hanušová <k.hanusova90@gmail.com>
2017-03-25 14:02:53 +0100

    FIX item buying

--- src/main/java/cz/hanusova/fingerprintgame/model/PlaceType.java
+++ src/main/java/cz/hanusova/fingerprintgame/model/PlaceType.java
@@ -4,8 +4,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
@@ -18,7 +16,6 @@ import org.hibernate.annotations.Immutable;
 public class PlaceType {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "ID_PLACE_TYPE")
 	private Long idPlaceType;
 
