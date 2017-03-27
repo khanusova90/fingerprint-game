6a140dfcf150a266f54b539ac553ee263e92114f
Kateřina Hanušová <k.hanusova90@gmail.com>
2017-03-27 16:36:12 +0200

    REST registration: refactor

--- src/main/java/cz/hanusova/fingerprintgame/controller/rest/MobileLoginController.java
+++ src/main/java/cz/hanusova/fingerprintgame/controller/rest/MobileLoginController.java
@@ -1,15 +1,8 @@
 package cz.hanusova.fingerprintgame.controller.rest;
 
-import java.util.Base64;
-import java.util.List;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -17,11 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
 
-import cz.hanusova.fingerprintgame.model.StagUser;
-import cz.hanusova.fingerprintgame.model.TimetableAction;
+import cz.hanusova.fingerprintgame.dto.UserDTO;
 import cz.hanusova.fingerprintgame.service.UserService;
 
 @RestController
@@ -30,8 +20,6 @@ public class MobileLoginController {
 
 	private static final Log logger = LogFactory.getLog(MobileLoginController.class);
 
-	private static final String STAG_URL = "https://stagws.uhk.cz/ws/services/rest/";
-
 	@Autowired
 	private UserService userService;
 
@@ -39,37 +27,13 @@ public class MobileLoginController {
 	public ResponseEntity<Object> getUser(@RequestParam("username") String username,
 			@RequestHeader("authorization") String auth) {
 		logger.info("Getting user from mobile app: " + username);
-		try {
-			RestTemplate template = new RestTemplate();
-			List<StagUser> users = template
-					.exchange(STAG_URL + "users/getOsobniCislaByExternalLogin?login=" + username + "&outputFormat=JSON",
-							HttpMethod.GET, null, new ParameterizedTypeReference<List<StagUser>>() {
-							})
-					.getBody();
-			if (users != null && users.size() > 0) {
-				HttpHeaders headers = new HttpHeaders();
-				headers.add("authorization", auth);
 
-				HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-				List<TimetableAction> timetable = template.exchange(
-						STAG_URL + "rozvrhy/getRozvrhByStudent?osCislo=" + users.get(0).getUserNumbers().get(0)
-								+ "&outputFormat=JSON",
-						HttpMethod.GET, entity, new ParameterizedTypeReference<List<TimetableAction>>() {
-						}).getBody();
-				if (timetable != null) {
-					byte[] credentialsByte = Base64.getDecoder().decode(auth.substring(auth.indexOf(" ") + 1));
-					String[] credentials = new String(credentialsByte).split(":");
-					// return
-					// userService.getUserDTOByUsernameAndPassword(credentials[0],
-					// credentials[1]);
-					return new ResponseEntity<Object>(
-							userService.getUserDTOByUsernameAndPassword(credentials[0], credentials[1]), HttpStatus.OK);
-				}
-			}
-		} catch (HttpClientErrorException e) {
-			logger.warn("Returned 401 from STAG");
+		UserDTO user = userService.loginUser(auth, username);
+		if (user == null) {
+			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+		} else {
+			return new ResponseEntity<Object>(user, HttpStatus.OK);
 		}
-		return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
 	}
 
 }
