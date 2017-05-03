d2197ba4c424ff9f8c373a954ffe09a4c08426f4
Kateřina Hanušová <k.hanusova90@gmail.com>
2017-05-03 20:04:52 +0200

    656469: save fingerprint to couchbase

--- src/main/java/cz/hanusova/fingerprintgame/service/impl/FingerprintServiceImpl.java
+++ src/main/java/cz/hanusova/fingerprintgame/service/impl/FingerprintServiceImpl.java
@@ -15,6 +15,13 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.CouchbaseCluster;
+import com.couchbase.client.java.document.JsonDocument;
+import com.couchbase.client.java.document.json.JsonObject;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import cz.hanusova.fingerprintgame.fingerprint.Fingerprint;
 import cz.hanusova.fingerprintgame.service.FingerprintService;
 
@@ -30,14 +37,33 @@ public class FingerprintServiceImpl implements FingerprintService {
 
 	private static final SimpleDateFormat SDF = new SimpleDateFormat("ddMMyyyy_HHmmssSSS");
 	private static final String SEPARATOR = "_";
+	private static final String ID_NAME = "fingerprint";
+	private static final String APP_NAME = "fingerprint-game";
+
+	private static int fingerprint_id = 0;
 
 	@Value("${file.fingerprint.name}")
 	private String fileName;
 	@Value("${file.fingerprint.path}")
 	private String path;
+	@Value("${profile.name}")
+	private String profile;
 
 	@Override
 	public void saveFingerprint(Fingerprint fingerprint) {
+		fingerprint.setAppName(APP_NAME);
+		saveToFile(fingerprint);
+		if (profile.equals("production")) {
+			saveToCouchbase(fingerprint);
+		}
+	}
+
+	/**
+	 * Saves fingerprint to new separate file
+	 * 
+	 * @param fingerprint
+	 */
+	private void saveToFile(Fingerprint fingerprint) {
 		Date now = new Date();
 		String name = path + fileName + SEPARATOR + SDF.format(now);
 		logger.info("Saving new fingerprint to file " + name);
@@ -47,7 +73,21 @@ public class FingerprintServiceImpl implements FingerprintService {
 		} catch (IOException e) {
 			logger.error("Could not save fingerprint to file", e);
 		}
+	}
 
+	private void saveToCouchbase(Fingerprint fingerprint) {
+		CouchbaseCluster cluster = CouchbaseCluster.create();
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			String fpString = mapper.writeValueAsString(fingerprint);
+			Bucket bucket = cluster.openBucket("beacon");
+			JsonObject object = JsonObject.fromJson(fpString);
+			bucket.upsert(JsonDocument.create(ID_NAME + fingerprint_id, object));
+			fingerprint_id++;
+		} catch (JsonProcessingException e) {
+			logger.error("could not deserialize fingerprint", e);
+		}
+		cluster.disconnect();
 	}
 
 }
